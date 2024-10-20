df80e2289642fae964ab1d8060d05f40e59baf8d
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2024-10-20 10:08:54 -0300

    chore(deps): add rainbow and wagmi dependecies and start config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,31 @@
 import "@/styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+
 import type { AppProps } from "next/app";
 
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { sepolia } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { useMemo } from "react";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const queryClient = useMemo(() => new QueryClient(), []);
+
+  const config = getDefaultConfig({
+    appName: "Bleu Challenge",
+    projectId: "YOUR_PROJECT_ID",
+    chains: [sepolia],
+    ssr: true, // If your dApp uses server side rendering (SSR)
+  });
+
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 }
