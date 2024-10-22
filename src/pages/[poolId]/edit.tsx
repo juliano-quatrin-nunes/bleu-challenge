0f1438d2a98a10906942462e8aee3f726d5242ca
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2024-10-22 07:38:09 -0300

    feat: test send metadata cid to pool

--- src/pages/[poolId]/edit.tsx
+++ src/pages/[poolId]/edit.tsx
@@ -1,15 +1,36 @@
+import Button from "@/components/Button/Button";
 import Footer from "@/components/EditMetadata/Footer/Footer";
 import EditHeader from "@/components/EditMetadata/Header/EditHeader";
 import List from "@/components/EditMetadata/List/List";
 import PoolHeader from "@/components/PoolHeader/PoolHeader";
 import styles from "@/styles/Pool.module.css";
+import { abi } from "@/utils/abi";
 import { convertRecordToMetadataArray, FormValues } from "@/utils/utils";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { Form, Formik } from "formik";
 import { useRouter } from "next/router";
+import { useWriteContract } from "wagmi";
 
 type MetaDataType = Record<string, string>;
 
+const poolIdHex =
+  "0xe13ab90f74c371ac2e8a531bf80f08b4d90cffff000200000000000000000161";
+const contractId = "0x61FD2dedA9c8a1ddb9F3F436D548C58643936f02";
+const CID = "QmP6KNc2rwd84Li3iQApfzeGM4HA8w5q3Cr6fgjSSkf7QB";
+
 const Visualize = () => {
+  const { writeContract, data, error, isSuccess } = useWriteContract();
+
+  console.log(data, isSuccess, error, CID.length);
+
+  const writeCIDtoContract = () =>
+    writeContract({
+      abi: abi,
+      address: contractId,
+      functionName: "setPoolMetadata",
+      args: [poolIdHex, CID],
+    });
+
   const router = useRouter();
   const poolId = router.query?.poolId as string;
 
@@ -27,6 +48,8 @@ const Visualize = () => {
   return (
     <div className={styles.container}>
       <div className={styles.poolInfo}>
+        <Button onClick={writeCIDtoContract}>write cid</Button>
+        <ConnectButton />
         <PoolHeader poolId={poolId} />
 
         <Formik<FormValues>
