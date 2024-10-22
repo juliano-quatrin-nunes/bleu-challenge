5e294c48352104c093f509413e75a4e551f9a22c
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2024-10-22 08:31:44 -0300

    fix: add redirect function to edit button

--- src/components/VisualizeMetadata/Header/VisualizeHeader.tsx
+++ src/components/VisualizeMetadata/Header/VisualizeHeader.tsx
@@ -1,11 +1,19 @@
 import Button from "@/components/Button/Button";
 import styles from "./VisualizeHeader.module.css";
+import { useRouter } from "next/router";
 
 const VisualizeHeader = () => {
+  const router = useRouter();
+
   return (
     <div className={styles.spaceBetweenRow}>
       <h3>View Metadata</h3>
-      <Button primary />
+      <Button
+        primary
+        onClick={() => router.push(`/${router.query.poolId}/edit`)}
+      >
+        Edit
+      </Button>
     </div>
   );
 };
