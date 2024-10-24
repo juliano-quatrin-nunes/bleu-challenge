bde18e792e3b0814fd7309a17229c7806f8cdb0e
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2024-10-23 21:24:25 -0300

    fix: prevent from wallet disconnet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,15 +10,15 @@ import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import { useMemo } from 'react'
 import Layout from '@/layouts/layout'
 
+const config = getDefaultConfig({
+  appName: 'Bleu Challenge',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [sepolia],
+  ssr: true,
+})
+
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = useMemo(() => new QueryClient(), [])
-
-  const config = getDefaultConfig({
-    appName: 'Bleu Challenge',
-    projectId: 'YOUR_PROJECT_ID',
-    chains: [sepolia],
-    ssr: true, // If your dApp uses server side rendering (SSR)
-  })
 
   return (
     <WagmiProvider config={config}>
