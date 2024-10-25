5b8e2c40b7ccaa1c7724ee571bf43708689c9a9a
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2024-10-25 09:56:44 -0300

    feat: enable user to enter a pool address to see and manage its metadata

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -2,16 +2,16 @@ import { DetailedHTMLProps, InputHTMLAttributes } from 'react'
 import styles from './TextField.module.css'
 
 interface FieldProps extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
-  label: string
+  label?: string
 }
 
 const TextField = (props: FieldProps) => {
-  const { label, ...rest } = props
+  const { label, className, ...rest } = props
 
   return (
     <div className={styles.verticalFlow}>
-      <label className={styles.label}>{label}</label>
-      <input className={styles.inputField} {...rest}></input>
+      {label && <label className={styles.label}>{label}</label>}
+      <input className={`${styles.inputField} ${className}`} {...rest}></input>
     </div>
   )
 }
