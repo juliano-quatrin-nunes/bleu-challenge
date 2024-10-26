6ce193c46c9d791221e881ba9f700dcdbec3635f
Juliano Quatrin Nunes <julianoqnunes17@gmail.com>
2024-10-26 09:57:45 -0300

    fix: show no metadata message when file is empty

--- frontend/src/components/VisualizeMetadata/Grid/Grid.tsx
+++ frontend/src/components/VisualizeMetadata/Grid/Grid.tsx
@@ -9,13 +9,17 @@ interface GridProps {
 }
 
 const Grid = ({ poolId }: GridProps) => {
-  const { data: cid, isSuccess, isLoading: isLoadingCid } = useGetPoolMetadataCid(poolId)
-  const { data: metadata, isLoading: isLoadingMetadata } = useFetchIpfsJson(cid)
+  const { data: cid, isSuccess: isSuccessCid, isLoading: isLoadingCid } = useGetPoolMetadataCid(poolId)
+  const { data: metadata, isSuccess: isSuccessMetadata, isLoading: isLoadingMetadata } = useFetchIpfsJson(cid)
 
-  if (isLoadingCid || isLoadingMetadata) return <div className={styles.metadataGrid}>Loading metadata info...</div>
-  if (isSuccess && !cid) return <div className={styles.metadataGrid}>This pool has no metadata</div>
+  const isAnyQueryLoading = isLoadingCid || isLoadingMetadata
+  const hasNoMetadataCid = isSuccessCid && !cid
+  const isFileEmpty = isSuccessMetadata && !Object.entries(metadata).length
 
-  if (metadata)
+  if (isAnyQueryLoading) return <div className={styles.metadataGrid}>Loading metadata info...</div>
+  if (hasNoMetadataCid || isFileEmpty) return <div className={styles.metadataGrid}>This pool has no metadata</div>
+
+  if (metadata && !isFileEmpty)
     return (
       <div className={styles.metadataGrid}>
         {Object.entries(metadata).map((record, index) => (
